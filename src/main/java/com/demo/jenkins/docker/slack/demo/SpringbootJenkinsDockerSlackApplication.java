616130eb1272ad6ae376a62b7af221a9281bc22f
gangdaewon <cskang8264@naver.com>
2021-04-07 14:05:03 +0900

    hihi

---
+++ src/main/java/com/demo/jenkins/docker/slack/demo/SpringbootJenkinsDockerSlackApplication.java
@@ -0,0 +1,13 @@
+package com.demo.jenkins.docker.slack.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsDockerSlackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJenkinsDockerSlackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/jenkins/docker/slack/demo/SpringbootJenkinsDockerSlackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.jenkins.docker.slack.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsDockerSlackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
