7b7a044bb6dc9f9aaa73814b1206586f9b877b7c
gangdaewon <cskang8264@naver.com>
2021-04-07 16:43:27 +0900

    github hook test2

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -4,10 +4,20 @@ package controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("test")
 public class TestController {
+
+    @GetMapping("/1")
+    public String testResponse() {
+        return "hihi";
+    }
+
 
 }
